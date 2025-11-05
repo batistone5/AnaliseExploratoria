d43665e2b7ca97a375804b8a60410584675a6ae1
batistone5 <samuelbatistone@id.uff.br>
2025-11-05 10:59:33 -0300

    Primeira versão do script com as análises gráficas.

---
+++ Script_1.R
@@ -0,0 +1,14 @@
+#Atividade de Prática Estatística sobre o GitHub
+
+#Ativando o Pacote para realizar ações no GitHub
+library(usethis)
+
+#Entrando com as minhas informações do GitHub
+use_git_config(
+  user.name = "batistone5",          
+  user.email = "samuelbatistone@id.uff.br"         
+)
+
+
+#1) O commit é usado para salvar as mudanças feitas no diretório do GitHub, podendo
+#incluir mensagens detalhando cada mudança, criando um hitórico das mudanças feitas.
